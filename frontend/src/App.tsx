a876f2bd3d54501e76e94cbf4d75104469c3c703
NPodpriatova <natalia.podpryatova@gmail.com>
2024-08-26 11:12:28 +0200

    create a movies list component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import './App.css';
-import IndividualMovie from './movies/IndividualMovie';
+import MoviesList from './movies/MoviesList';
+import { movieDTO } from './movies/movies.model';
 
 function App() {
-const testMovie =           {
+const inTheaters: movieDTO[] = [{
   id: 1,
   title: 'Spider-Man: Far From Home',
   poster: 'https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg'
-};
+},
+{
+  id: 2,
+  title: 'Luca',
+  poster: 'https://i0.wp.com/www.hd.com.do/wp-content/uploads/2021/04/1619620464_LUCA-Disney.png?fit=525%2C750&ssl=1'
+}];
+
+const upcomingReleases: movieDTO[] = [{
+    id: 3,
+    title: 'Soul',
+    poster: 'https://i.pinimg.com/originals/9d/6a/04/9d6a0489776b1abde5f7f1b474a8b6c4.png'
+  }
+]
 
   return (
     <>
-    <IndividualMovie {...testMovie} />
+    <h3>In Theaters</h3>
+    <MoviesList movies={inTheaters}/>
+
+    <h3>Upcoming Releases</h3>
+    <MoviesList movies={upcomingReleases}/>
     </>
   );
 }
